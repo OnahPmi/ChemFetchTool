f254ffe2290d3133200a7e21238604eddcef4f5d
Emmanuel Onah <onahemma111@gmail.com>
2024-03-21 21:33:05 +0100

    Modified the App.py

--- App.py
+++ App.py
@@ -238,6 +238,6 @@ with col_a:
   st.write("""
     If you make use of **:rainbow[ChemFetchTool]** in your work, cite it as follows:
 
-    **Emmanuel Onah. (2024). __OnahPmi/ChemFetchTool: ChemFetchTool (v1.0.0)__. Zenodo. https://doi.org/10.5281/zenodo.10850253.**
+    **Onah, E. (2024). ChemFetchTool (Version 1.1.0) [Computer software]. https://doi.org/10.5281/zenodo.10850253.**
   """)
   st.divider()
