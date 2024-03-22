faa661220943902fd56d1da55619887f9589d21c
Emmanuel Onah <onahemma111@gmail.com>
2024-03-22 15:35:11 +0100

    Adjusted the refencing style on the App page

--- App.py
+++ App.py
@@ -266,5 +266,5 @@ with col2:
         [https://jfas.info/index.php/JFAS/article/view/987](https://jfas.info/index.php/JFAS/article/view/987).
         """)
   st.markdown("""#### If you make use of **:rainbow[ChemFetchTool]** in your work, please cite it as follows:  
-  Onah, E. (2024). ChemFetchTool: Automate Molecular Properties Retrieval from PubChem (Version 1.1.0) Zenodo. https://doi.org/10.5281/zenodo.10850870.""")
+  Onah, E. (2024). ChemFetchTool: Automate Molecular Properties Retrieval from PubChem (Version 1.1.0) Zenodo. [10.5281/zenodo.10850870](https://doi.org/10.5281/zenodo.10850870).""")
 
