dcfef588f6bdb9b065faa70edfc50f08cf95dcc9
Emmanuel Onah <onahemma111@gmail.com>
2024-03-22 14:59:54 +0100

    Adjusted the refencing style on the App page

--- App.py
+++ App.py
@@ -266,4 +266,3 @@ with st.container():
           """)
     st.markdown("""#### If you make use of **:rainbow[ChemFetchTool]** in your work, please cite it as follows:  
     Onah, E. (2024). ChemFetchTool: Automate Molecular Properties Retrieval from PubChem (Version 1.1.0) Zenodo. https://doi.org/10.5281/zenodo.10850870.""")
-
